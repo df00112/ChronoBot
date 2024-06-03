de871549527fc6cb8f37a0d21a995ed357f6d87a
df00112 <df00112@hotmail.com>
2024-06-03 18:17:30 +0100

    added bullet, need animation for shooting

---
+++ Assets/Objects/Weapons/Ranged/Bullet.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 20f;
+    public float lifetime = 2f;
+    public int damage = 10;
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb.velocity = transform.forward * speed;
+        Destroy(gameObject, lifetime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            /* Enemy enemy = other.GetComponent<Enemy>();
+            if (enemy != null)
+            {
+                enemy.TakeDamage(damage);
+            } */
+            Destroy(gameObject);
+        }
+        else if (!other.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
