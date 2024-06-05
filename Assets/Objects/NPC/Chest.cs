d8b10d955a9af4ead48790738e70a2a004640a48
df00112 <df00112@hotmail.com>
2024-06-05 18:46:48 +0100

    added chest animation

--- Assets/Objects/NPC/Chest.cs
+++ Assets/Objects/NPC/Chest.cs
@@ -7,9 +7,24 @@ public class Chest : MonoBehaviour, IInteractable
     [SerializeField] private string _chestName;
     public string InteractionText => _chestName;
 
+    private Animator _animator;
+    //private AudioSource _audioSource;
+
+    void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        //_audioSource = GetComponent<AudioSource>();
+    }
+
     public bool Interact(Interactor interactor)
     {
-        Debug.Log("Chest opened");
+        
+        // Trigger the chest opening animation
+        // Play the chest opening sound
+        // Spawn loot
+
+        _animator.SetTrigger("Open");
+        //_audioSource.Play();
         return true;
     }
 }
